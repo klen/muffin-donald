8d591b117b1a60dbd3da89be03a3a885e82310fd
Kirill Klenov <horneds@gmail.com>
2021-05-28 16:38:27 +0300

    feat: initial commit

---
+++ tests.py
@@ -0,0 +1,32 @@
+import muffin
+import pytest
+from asgi_tools.tests import manage_lifespan
+
+
+@pytest.fixture
+def aiolib():
+    return ('asyncio', {'use_uvloop': False})
+
+
+@pytest.fixture
+def app():
+    return muffin.Application(DEBUG=True)
+
+
+async def task1():
+    return 42
+
+
+async def test_base(app):
+    from muffin_donald import Plugin
+
+    tasks = Plugin(app, num_workers=2)
+    assert tasks
+    assert tasks.donald
+
+    async with manage_lifespan(app):
+        assert tasks.donald._started
+        res = await tasks.submit(task1)
+        assert res == 42
+
+    assert not tasks.donald._started
