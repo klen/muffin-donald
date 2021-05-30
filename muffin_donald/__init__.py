125ec1612143ac4809c8b1f31ca1dc12ac65738e
Kirill Klenov <horneds@gmail.com>
2021-05-30 15:49:47 +0300

    Bump version: 0.0.5 → 0.0.6

--- muffin_donald/__init__.py
+++ muffin_donald/__init__.py
@@ -7,7 +7,7 @@ from muffin import Application
 from muffin.plugins import BasePlugin
 
 
-__version__ = "0.0.5"
+__version__ = "0.0.6"
 __project__ = "muffin-donald"
 __author__ = "Kirill Klenov <horneds@gmail.com>"
 __license__ = "MIT"
